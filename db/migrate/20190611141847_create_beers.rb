4438cb6fb2ff305866bd7aed22491d22fe60f86a
charneff <charlotte.neff@gmail.com>
2019-06-11 09:18:58 -0500

    beer setup

---
+++ db/migrate/20190611141847_create_beers.rb
@@ -0,0 +1,12 @@
+class CreateBeers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :beers do |t|
+      t.string :style
+      t.string :flavor_profile
+      t.belongs_to :brewery, foreign_key: true
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
