a836851e585d576aed41844a016e9c0ddd0877e1
charneff <charlotte.neff@gmail.com>
2019-06-11 09:30:21 -0500

    associations

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,6 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   belongs_to :user
+  has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :reviews
+  has_many :beers, through: :reviews
 end
