5c4d9b647de110ced7784ab0429776a5152fc97b
charneff <charlotte.neff@gmail.com>
2019-06-11 21:51:37 -0500

    create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,24 @@
 class ReviewsController < ApplicationController
 
   def new
-    @beer = Beer.find_by_id(params[:id])
-    @review = @beer.reviews.build 
+    @beer = Beer.find_by_id(params[:beer_id])
+    @review = @beer.reviews.build
+  end
+
+  def create
+    @review = Review.new(review_params)
+    redirect_to review_path(@review)
   end
 
   def index
     @reviews = Review.all
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:beer_id, :stars, :title. :content)
+  end
+
 
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,4 +4,8 @@ class Beer < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews #user who reviewed
   accepts_nested_attributes_for :brewery
+
+  def brewery_attributes=(attributes)
+    brewery = Brewery.find_or_create_by(attributes) if !name.empty?
+  end 
 end
